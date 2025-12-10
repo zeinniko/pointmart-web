05c55a9cb63d65313ec715d7a2f4b1f1abff81ec
zeinniko <zeinniko712@gmail.com>
2025-12-10 14:56:05 +0700

    cors config api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::options('/{any}', function () {
+    return response()->json([], 200, [
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers' => '*',
+    ]);
+})->where('any', '.*');
+
+
+
 // ✅ Gunakan semua controller
 use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\UserController;
