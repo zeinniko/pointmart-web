67d79ececc21c972a6e8ce5d22b873a4d3f6338f
zeinniko <zeinniko712@gmail.com>
2025-12-10 15:08:03 +0700

    allowed_origins

--- config/cors.php
+++ config/cors.php
@@ -2,24 +2,13 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://pointmart.cloud',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +18,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
-
+    'supports_credentials' => true,
 ];
+
