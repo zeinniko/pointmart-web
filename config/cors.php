e7c6fd687469bb952d1612d8b4b1dbc9dbef1603
zeinniko <zeinniko712@gmail.com>
2025-12-11 09:53:41 +0700

    add allowed_methods

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:18712'],
+    'allowed_origins' => [
+        'http://localhost:3000', // frontend dev
+        'https://pointmart.cloud', // frontend production
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +32,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
