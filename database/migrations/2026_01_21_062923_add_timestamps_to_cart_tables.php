e36a05448d31e590f2d3a8e1e3c610baa7a0950b
zeinniko <zeinniko712@gmail.com>
2026-01-21 13:32:53 +0700

    add timestamp to user address and cart

---
+++ database/migrations/2026_01_21_062923_add_timestamps_to_cart_tables.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            if (!Schema::hasColumn('carts', 'created_at')) {
+                $table->timestamp('created_at')->nullable();
+            }
+            if (!Schema::hasColumn('carts', 'updated_at')) {
+                $table->timestamp('updated_at')->nullable();
+            }
+        });
+
+        Schema::table('cart_items', function (Blueprint $table) {
+            if (!Schema::hasColumn('cart_items', 'created_at')) {
+                $table->timestamp('created_at')->nullable();
+            }
+            if (!Schema::hasColumn('cart_items', 'updated_at')) {
+                $table->timestamp('updated_at')->nullable();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            if (Schema::hasColumn('carts', 'created_at')) {
+                $table->dropColumn('created_at');
+            }
+            if (Schema::hasColumn('carts', 'updated_at')) {
+                $table->dropColumn('updated_at');
+            }
+        });
+
+        Schema::table('cart_items', function (Blueprint $table) {
+            if (Schema::hasColumn('cart_items', 'created_at')) {
+                $table->dropColumn('created_at');
+            }
+            if (Schema::hasColumn('cart_items', 'updated_at')) {
+                $table->dropColumn('updated_at');
+            }
+        });
+    }
+};
